d173538546f7d4b15463c29985e6da7f615d66ab
PomHaffs <tomashaffenden@icloud.com>
2017-01-03 11:15:45 +1100

    Added data structure

--- WhereAmI/PlacesViewController.swift
+++ WhereAmI/PlacesViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
+//global varible
+var places = [Dictionary<String, String>()]
+
 class PlacesViewController: UITableViewController {
 
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if places.count == 1 && places[0].count == 0 {
+            
+            places.remove(at: 0)
+            places.append(["name":"Taj Mahal", "lat":"27.175277", "long":"78.04128"])
+        }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+            table.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,24 +35,36 @@ class PlacesViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+      
+        return places.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
 
-        // Configure the cell...
+        if places[indexPath.row]["name"] != nil {
+            
+            cell.textLabel?.text = places[indexPath.row]["name"]
+            
+        }
+    
 
         return cell
     }
-    */
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "toMap", sender: nil)
+        
+    }
 
     /*
     // Override to support conditional editing of the table view.
